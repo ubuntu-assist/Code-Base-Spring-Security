e2edfe82f7d3eeeb44191f06cabeb005e49cb429
FOPA KUETE DUCLAIR <fopaduclair2000@gmail.com>
2025-03-21 17:00:03 +0100

    Finish with account creation flow

--- src/main/java/com/mock/io/auth/AuthenticationController.java
+++ src/main/java/com/mock/io/auth/AuthenticationController.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,26 +23,20 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("register")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest) throws MessagingException {
         authenticationService.register(registerRequest);
         return ResponseEntity.accepted().build();
     }
 
-//    @PostMapping("authenticate")
-//    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
-//        return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
-//    }
-//
-//    @PostMapping("refresh-token")
-//    public void refreshToken(
-//            HttpServletRequest request,
-//            HttpServletResponse response
-//    ) throws IOException {
-//        authenticationService.refreshToken(request, response);
-//    }
-//
-//    @GetMapping("confirm")
-//    public ResponseEntity<String> confirm(@RequestParam String token) {
-//        return ResponseEntity.ok(authenticationService.confirm(token));
-//    }
+    @PostMapping("authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
+        return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
+    }
+
+
+    @GetMapping("activate-account")
+    public void confirm(@RequestParam String token) throws MessagingException {
+        authenticationService.activateAccount(token);
+    }
 }
--- src/main/java/com/mock/io/auth/AuthenticationService.java
+++ src/main/java/com/mock/io/auth/AuthenticationService.java
@@ -1,6 +1,5 @@
 package com.mock.io.auth;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mock.io.email.EmailService;
 import com.mock.io.email.EmailTemplateName;
 import com.mock.io.role.Role;
@@ -12,20 +11,18 @@ import com.mock.io.user.User;
 import com.mock.io.user.UserRepository;
 import com.mock.io.validators.ObjectsValidator;
 import jakarta.mail.MessagingException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -89,7 +86,7 @@ public class AuthenticationService {
     }
 
     private String generateAndSaveActivationToken(User user) {
-        String generatedToken = generateActivationCode(6);
+        String generatedToken = generateActivationCode();
         Token token = Token.builder()
                 .token(generatedToken)
                 .createdAt(LocalDateTime.now())
@@ -100,11 +97,11 @@ public class AuthenticationService {
         return generatedToken;
     }
 
-    private String generateActivationCode(int length) {
+    private String generateActivationCode() {
         String characters = "0123456789";
         StringBuilder codeBuilder = new StringBuilder();
         SecureRandom secureRandom = new SecureRandom();
-        for(int i = 0; i < length; i++) {
+        for(int i = 0; i < 6; i++) {
             int randomIndex = secureRandom.nextInt(characters.length());
             codeBuilder.append(characters.charAt(randomIndex));
         }
@@ -119,138 +116,39 @@ public class AuthenticationService {
      * @throws IllegalStateException if the provided email is not found in the database
      * @throws UsernameNotFoundException if the user with the provided email is not found
      */
-//    public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
-//        authenticationManager.authenticate(
-//                new UsernamePasswordAuthenticationToken(
-//                        authenticationRequest.email(),
-//                        authenticationRequest.password()
-//                )
-//        );
-//
-//        User user = userRepository.findByEmail(authenticationRequest.email())
-//                .orElseThrow(() -> new IllegalStateException("User with email [%s] not found".formatted(authenticationRequest.email())));
-//
-//        // Tokens generation
-//        String jwtToken = jwtService.generateToken(user);
-//        String refreshToken = jwtService.generateRefreshToken(user);
-//
-//        revokeAllUserTokens(user);
-//
-//        saveUserToken(user, jwtToken);
-//
-//        return new AuthenticationResponse(jwtToken, refreshToken);
-//    }
+    public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
+        var auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.email(),
+                        authenticationRequest.password()
+                )
+        );
 
-    /**
-     * Revokes all tokens associated with the given user.
-     *
-     * @param user the user whose tokens should be revoked
-     */
-//    private void revokeAllUserTokens(User user) {
-//        List<Token> validUserTokens = tokenRepository.findAllValidTokensByUserId(user.getId());
-//
-//        if(validUserTokens.isEmpty()) return;
-//
-//        validUserTokens.forEach(token -> token.setExpiresAt(LocalDateTime.now()));
-//        tokenRepository.saveAll(validUserTokens);
-//    }
+        var claims = new HashMap<String, Object>();
+        User user = ((User)auth.getPrincipal());
+        claims.put("fullname", user.fullName());
+        String jwtToken = jwtService.generateToken(claims, user);
 
-    /**
-     * Saves a new token for the given user.
-     *
-     * @param user the user whose token should be saved
-     * @param jwtToken the JWT token to be saved
-     */
-    private void saveUserToken(User user, String jwtToken) {
-        Token token = Token
-                .builder()
-                .token(jwtToken)
-                .createdAt(LocalDateTime.now())
-                .expiresAt(LocalDateTime.now().plusMinutes(15))
-                .user(user)
-                .build();
-
-        tokenRepository.save(token);
+        return new AuthenticationResponse(jwtToken);
     }
 
-    /**
-     * Refreshes the user's token.
-     *
-     * @param request  the HTTP request containing the authorization header
-     * @param response the HTTP response to send the new access token
-     * @throws IOException if an error occurs while writing the response
-     */
-//    public void refreshToken(
-//            HttpServletRequest request,
-//            HttpServletResponse response
-//    ) throws IOException {
-//        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-//
-//        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-//            return;
-//        }
-//
-//        final String refreshToken = authHeader.substring(7);
-//        final String userEmail = jwtService.extractUserName(refreshToken);
-//
-//        if (userEmail != null) {
-//            User user = userRepository.findByEmail(userEmail)
-//                    .orElseThrow(() -> new IllegalStateException("User not found"));
-//
-//            if (jwtService.isTokenValid(refreshToken, user)) {
-//                String accessToken = jwtService.generateToken(user);
-//                revokeAllUserTokens(user);
-//                saveUserToken(user, accessToken);
-//
-//                AuthenticationResponse authenticationResponse = new AuthenticationResponse(
-//                        accessToken,
-//                        refreshToken
-//                );
-//
-//                new ObjectMapper().writeValue(response.getOutputStream(), authenticationResponse);
-//            }
-//        }
-//    }
 
-    /**
-     * Confirms the user's email address.
-     *
-     * @param token the token used to confirm the user's email address
-     * @return a confirmation message indicating whether the account has been successfully activated
-     * @throws IllegalStateException if the token is not found
-     * @throws UsernameNotFoundException if the user with the token is not found
-     */
-//    public String confirm(String token) {
-//        Token savedToken = tokenRepository.findByToken(token)
-//                .orElseThrow(() -> new IllegalStateException("Token not found"));
-//
-//        if (savedToken.isExpired()) {
-//            String jwtToken = jwtService.generateToken(savedToken.getUser());
-//            String refreshToken = jwtService.generateRefreshToken(savedToken.getUser());
-//
-//            revokeAllUserTokens(savedToken.getUser());
-//            saveUserToken(savedToken.getUser(), jwtToken);
-//            try {
-//                emailService.sendEmail(
-//                        savedToken.getUser().getEmail(),
-//                        savedToken.getUser().getFirstName(),
-//                        "confirm-email",
-//                        String.format(CONFIRMATION_URL, jwtToken)
-//                );
-//            } catch (MessagingException e) {
-//                throw new RuntimeException(e);
-//            }
-//
-//            return "Token expired, a new token has been send to your email";
-//        }
-//
-//        User user = userRepository.findById(savedToken.getUser().getId())
-//                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-//        user.setEnabled(true);
-//        userRepository.save(user);
-//
-//        savedToken.setValidatedAt(LocalDateTime.now());
-//        tokenRepository.save(savedToken);
-//        return "<h1>Your account has been successfully activated</h1>";
-//    }
+    @Transactional
+    public void activateAccount(String token) throws MessagingException {
+        Token savedToken = tokenRepository.findByToken(token)
+                .orElseThrow(() -> new RuntimeException("Invalid token"));
+
+        if (LocalDateTime.now().isAfter(savedToken.getExpiresAt())) {
+            sendValidationEmail(savedToken.getUser());
+            throw new RuntimeException("Activation token as expired. A new token has been sent to the same email address.");
+        }
+
+        User user = userRepository.findById(savedToken.getUser().getId())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        user.setEnabled(true);
+        userRepository.save(user);
+
+        savedToken.setValidatedAt(LocalDateTime.now());
+        tokenRepository.save(savedToken);
+    }
 }
